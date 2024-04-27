eaff726807a7a19861e567cae961f53274cc5c67
Daniel Gołębiewski <daniel.golebiewski@outlook.com>
2024-04-27 12:38:34 +0200

    Add tmux integration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,6 +13,9 @@ return {
 			})
 		end,
 	},
+  {
+    "preservim/nerdcommenter"
+  },
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
@@ -34,12 +37,12 @@ return {
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<space>f", function()
+			vim.keymap.set("n", "<leader>f", function()
 				vim.lsp.buf.format({ async = true })
 			end, {})
-			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+			--vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
 			vim.keymap.set("n", "rn", vim.lsp.buf.rename, {})
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
 }
