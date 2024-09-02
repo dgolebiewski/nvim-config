6994bbd831c4fca045f9b53cc217e36db0f30fdc
Daniel Gołębiewski <daniel.golebiewski@outlook.com>
2024-09-02 22:11:44 +0200

    Add tailwind

---
+++ lua/plugins/tailwind.lua
@@ -0,0 +1,4 @@
+return {
+  "lockasRanarison/tailwind-tools.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,30 @@
-return { 
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_install = true,
-      ensure_installed = {"lua", "javascript", "go"},
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			ensure_installed = { "lua", "javascript", "go" },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+
+		local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+		parser_config.blade = {
+			install_info = {
+				url = "https:/github.com/EmranMR/tree-sitter-blade",
+				files = { "src/parser.c" },
+				branch = "main",
+			},
+			filetype = "blade",
+		}
+
+		vim.filetype.add({
+			pattern = {
+				[".*%.blade%.php"] = "blade",
+			},
+		})
+	end,
 }
