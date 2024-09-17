2491882e812bc757370d299d67527ba6278b1d02
Daniel Gołębiewski <daniel.golebiewski@outlook.com>
2024-09-17 11:30:20 +0200

    Image preview, ts lsp update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,7 +6,25 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config = function() 
+  config = function()
+    require('neo-tree').setup({
+      filesystem = {
+        window = {
+          mappings = {
+            ["<leader>p"] = "image_preview", -- " or another map
+          },
+        },
+        commands = {
+          image_preview = function(state)
+            local node = state.tree:get_node()
+            if node.type == "file" then
+              require("image_preview").PreviewImage(node.path)
+            end
+          end,
+        },
+      },
+    })
+
     vim.keymap.set('n', '<C-a>', ':Neotree filesystem reveal left<CR>', {})
   end
 }
